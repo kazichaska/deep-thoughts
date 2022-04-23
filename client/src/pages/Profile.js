7aa4ca06a82eba68613227d6f4bd3554b65fe2a9
Kazi Islam <kazichaska@gmail.com>
2022-04-22 19:51:14 -0500

    added thoughtForm component

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Redirect, useParams } from 'react-router-dom';
 import ThoughtList from '../components/ThoughtList';
-import { useQuery } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
 import FriendList from '../components/FriendList';
 import Auth from '../utils/auth';
+import { ADD_FRIEND } from '../utils/mutations';
+import ThoughtForm from '../components/ThoughtForm';
+
 
 const Profile = () => {
   const { username: userParam } = useParams();
+  const [addFriend] = useMutation(ADD_FRIEND);
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam }
   });
@@ -29,17 +33,32 @@ const Profile = () => {
     );
   }
 
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id }
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
   return (
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
           Viewing {useParams ? `${user.username}'s` : 'your'} profile.
         </h2>
+        {useParams && (
+          <button className="btn ml-auto" onClick={handleClick}>
+            Add Friend
+          </button>
+        )}
       </div>
 
       <div className="flex-row justify-space-between mb-3">
         <div className="col-12 mb-3 col-lg-8">{/* PRINT THOUGHT LIST  */}</div>
-          <ThoughtList thoughts={user.thoughts} title={`${user.username}'s thoughts...`} />
+        <ThoughtList thoughts={user.thoughts} title={`${user.username}'s thoughts...`} />
         <div className="col-12 col-lg-3 mb-3">{/* PRINT FRIEND LIST */}
           <FriendList
             username={user.username}
@@ -48,6 +67,7 @@ const Profile = () => {
           />
         </div>
       </div>
+      <div className='mb-3'>{!userParam && <ThoughtForm />}</div>
     </div>
   );
 };
